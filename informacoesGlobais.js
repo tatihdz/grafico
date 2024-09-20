fe0b76a7c49dd8d53ff0826bb1eb1582a3cef1c4
tatihdz <119941402+tatihdz@users.noreply.github.com>
2024-09-20 12:14:28 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -4,6 +4,9 @@ async function VizualizarInformaçõesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem `
 }
 
 VizualizarInformaçõesGlobais()
